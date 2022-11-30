9fbfa804f27e37dc7ad2969291ac798f23597ccd
Chang Liu <chang.liu52@icloud.com>
2022-11-30 16:32:45 +0000

    Modification for Enabling control from Bio_Ik

---
+++ extend_gripper_control_xarm/scripts/gripper_control_digital.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rosnode
+import rospy
+import sys
+import copy
+import rospkg
+import extend_msgs
+from extend_msgs.msg import GripperControl
+from std_msgs.msg import String
+import xarm_msgs.srv
+
+#Creating the ros node and service client
+rospy.init_node("xarm_gripper")
+rospy.wait_for_service("/xarm/gripper_config")
+rospy.wait_for_service("/xarm/gripper_move")
+
+def dataCallback(msg):
+    # Remaping Range [0,1] to [0,850]
+    gripper_value = 850 + (-850 * msg.gripper_analog.data)
+    gripper_control = rospy.ServiceProxy("/xarm/gripper_move", xarm_msgs.srv.GripperMove)
+    gripper_action = gripper_control(gripper_value)
+    
+if __name__ == '__main__': 
+    #Subscribe to Digital Gripper Data Stream from Unity  
+    gripper_speed_service = rospy.ServiceProxy("/xarm/gripper_config", xarm_msgs.srv.GripperConfig)
+    gripper_speed_value = gripper_speed_service(5000)
+    rospy.Subscriber("gripper_control_digital", GripperControl, dataCallback)
+    rospy.spin() 
