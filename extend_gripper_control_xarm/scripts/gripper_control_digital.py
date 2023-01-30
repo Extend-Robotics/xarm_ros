95e0e7125423fd2ce39bfa2a1eaebe3225a8eb8e
Chang Liu <chang.liu52@icloud.com>
2023-01-30 13:26:49 +0000

    Update the Gripper Controller

--- extend_gripper_control_xarm/scripts/gripper_control_digital.py
+++ extend_gripper_control_xarm/scripts/gripper_control_digital.py
@@ -17,7 +17,7 @@ rospy.wait_for_service("/xarm/gripper_move")
 
 def dataCallback(msg):
     # Remaping Range [0,1] to [0,850]
-    gripper_value = 850 + (-850 * msg.gripper_analog.data)
+    gripper_value = 850 + (-850 * msg.gripperAnalog.data)
     gripper_control = rospy.ServiceProxy("/xarm/gripper_move", xarm_msgs.srv.GripperMove)
     gripper_action = gripper_control(gripper_value)
     
@@ -25,5 +25,5 @@ if __name__ == '__main__':
     #Subscribe to Digital Gripper Data Stream from Unity  
     gripper_speed_service = rospy.ServiceProxy("/xarm/gripper_config", xarm_msgs.srv.GripperConfig)
     gripper_speed_value = gripper_speed_service(5000)
-    rospy.Subscriber("gripper_control_digital", GripperControl, dataCallback)
+    rospy.Subscriber("extend_gripper_command", GripperControl, dataCallback)
     rospy.spin() 
