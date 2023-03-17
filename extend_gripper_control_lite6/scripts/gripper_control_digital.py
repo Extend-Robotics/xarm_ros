e36f1f7c038d66c0ffe63a082407f6708685702f
Nitish <nitish.sharma@extendrobotics.com>
2023-03-17 10:18:41 +0000

    Updates to the gripper control script for lite6

--- extend_gripper_control_lite6/scripts/gripper_control_digital.py
+++ extend_gripper_control_lite6/scripts/gripper_control_digital.py
@@ -17,16 +17,16 @@ rospy.wait_for_service("/ufactory/open_lite6_gripper")
 
 def dataCallback(msg):
     # Remaping Range [0,1] to [0,850]
-    if(msg.gripperDigital):
+    if msg.gripperDigital.data:
         gripperControl = rospy.ServiceProxy("/ufactory/close_lite6_gripper", xarm_msgs.srv.Call)
-        gripperControl()
+        gripperAction = gripperControl()
     else:
         gripperControl = rospy.ServiceProxy("/ufactory/open_lite6_gripper", xarm_msgs.srv.Call)
-        gripperControl()
-    
-if __name__ == '__main__': 
-    #Subscribe to Digital Gripper Data Stream from Unity  
+        gripperAction = gripperControl()
+
+if __name__ == '__main__':
+    #Subscribe to Digital Gripper Data Stream from Unity
     #gripper_speed_service = rospy.ServiceProxy("/xarm/gripper_config", xarm_msgs.srv.GripperConfig)
     #gripper_speed_value = gripper_speed_service(5000)
     rospy.Subscriber("extend_gripper_command", GripperControl, dataCallback)
-    rospy.spin() 
+    rospy.spin()
