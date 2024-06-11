3b70fd688400c4453ebb9e0cdf40c11ac9351118
nitish3693 <75649112+nitish3693@users.noreply.github.com>
2024-06-11 13:09:40 +0100

    Update gripper_control_inspire.py

--- extend_gripper_control_xarm/scripts/gripper_control_inspire.py
+++ extend_gripper_control_xarm/scripts/gripper_control_inspire.py
@@ -19,32 +19,30 @@ rospy.wait_for_service("/xarm/getset_tgpio_modbus_data")
 
 
 def dataCallback(msg):
+    if len(msg.handJointValues) > 0:
+        # Remaping Range [0,1] to [0,2000]
+        lowOrderLittleFinger,highOrderLittleFinger = split_decimal(int(round(20.62*msg.handJointValues[0])))
+        lowOrderRingFinger,highOrderRingFinger = split_decimal(int(round(20.62*msg.handJointValues[1])))
+        lowOrderMiddleFinger,highOrderMiddleFinger = split_decimal(int(round(20.62*msg.handJointValues[2])))
+        lowOrderIndexFinger,highOrderIndexFinger = split_decimal(int(round(20.62*msg.handJointValues[3])))
+        lowOrderThumbFinger,highOrderThumbFinger = split_decimal(int(round(51.282*(msg.handJointValues[4]+6))))
+        lowOrderThumbBaseFinger,highOrderThumbBaseFinger = split_decimal(int(round(25*(msg.handJointValues[5]+6))))
 
-    # Remaping Range [0,1] to [0,2000]
-    lowOrderLittleFinger,highOrderLittleFinger = split_decimal(int(round(20.62*msg.handJointValues[0])))
-    lowOrderRingFinger,highOrderRingFinger = split_decimal(int(round(20.62*msg.handJointValues[1])))
-    lowOrderMiddleFinger,highOrderMiddleFinger = split_decimal(int(round(20.62*msg.handJointValues[2])))
-    lowOrderIndexFinger,highOrderIndexFinger = split_decimal(int(round(20.62*msg.handJointValues[3])))
-    lowOrderThumbFinger,highOrderThumbFinger = split_decimal(int(round(51.282*(msg.handJointValues[4]+6))))
-    lowOrderThumbBaseFinger,highOrderThumbBaseFinger = split_decimal(int(round(25*(msg.handJointValues[5]+6))))
-
-    gripper_modbus_service = rospy.ServiceProxy("/xarm/getset_tgpio_modbus_data", xarm_msgs.srv.GetSetModbusData)
-    gripper_modbus_data = GetSetModbusDataRequest()
-    little_finger = 500
-    low_order, high_order = split_decimal(little_finger)
-    gripper_modbus_data.send_data = [0x01, 0x06, 0x05, 0xC2,
+        gripper_modbus_service = rospy.ServiceProxy("/xarm/getset_tgpio_modbus_data", xarm_msgs.srv.GetSetModbusData)
+        gripper_modbus_data = GetSetModbusDataRequest()
+        gripper_modbus_data.send_data = [0x01, 0x06, 0x05, 0xC2,
                                      highOrderLittleFinger,lowOrderLittleFinger,
                                      highOrderRingFinger,lowOrderRingFinger,
                                      highOrderMiddleFinger,lowOrderMiddleFinger,
                                      highOrderIndexFinger,lowOrderIndexFinger,
                                      highOrderThumbFinger,lowOrderThumbFinger, 
                                      highOrderThumbBaseFinger,lowOrderThumbBaseFinger]
-    gripper_modbus_data.respond_len = 6
-    gripper_modbus_data.host_id = 9
-    gripper_modbus_data.is_transparent_transmission = False
-    gripper_modbus_data.use_503_port = False
-    gripper_modbus_service(gripper_modbus_data) 
-
+        gripper_modbus_data.respond_len = 6
+        gripper_modbus_data.host_id = 9
+        gripper_modbus_data.is_transparent_transmission = False
+        gripper_modbus_data.use_503_port = False
+        gripper_modbus_service(gripper_modbus_data) 
+        
 
 
 def split_decimal(decimal):
@@ -64,12 +62,11 @@ def split_decimal(decimal):
 
 if __name__ == '__main__': 
     #Configure the Baudrate for the tool modbus
-    print("Starting the script to control Inspire Hand")
-    gripper_baudrate_service = rospy.ServiceProxy("/xarm/config_tool_modbus", xarm_msgs.srv.ConfigToolModbus)
-    gripper_baudrate_config = ConfigToolModbusRequest()
-    gripper_baudrate_config.baud_rate = 115200  #Baudrate for the Robotiq grippers
-    gripper_baudrate_config.timeout_ms = 3
-    gripper_baudrate_service(gripper_baudrate_config)    
+    # gripper_baudrate_service = rospy.ServiceProxy("/xarm/config_tool_modbus", xarm_msgs.srv.ConfigToolModbus)
+    # gripper_baudrate_config = ConfigToolModbusRequest()
+    # gripper_baudrate_config.baud_rate = 115200  #Baudrate for the Robotiq grippers
+    # gripper_baudrate_config.timeout_ms = 3
+    # gripper_baudrate_service(gripper_baudrate_config)    
 
     # #Reset Gripper 
     gripper_modbus_service = rospy.ServiceProxy("/xarm/getset_tgpio_modbus_data", xarm_msgs.srv.GetSetModbusData)
@@ -80,19 +77,7 @@ if __name__ == '__main__':
     gripper_modbus_data.is_transparent_transmission = False
     gripper_modbus_data.use_503_port = False
     gripper_modbus_service(gripper_modbus_data) 
-
-    #Activate Gripper 
-    # gripper_modbus_data = GetSetModbusDataRequest()
-    # gripper_modbus_data.send_data = [0x01, 0x06, 0x05, 0xCE, 0x00, 0x00]
-    # gripper_modbus_data.respond_len = 6
-    # gripper_modbus_data.host_id = 9
-    # gripper_modbus_data.is_transparent_transmission = False
-    # gripper_modbus_data.use_503_port = False
-    # response = gripper_modbus_service(gripper_modbus_data)  
-    # print("request : 01 06 05 CE 00 00 E8 F9 ")
-    # print(response)
    
-    #Subscribe to Digital Gripper Data Stream from Unity  
+    # #Subscribe to Digital Gripper Data Stream from Unity  
     rospy.Subscriber("/extend_gripper_command", GripperControl, dataCallback, queue_size=1)
     rospy.spin() 
-
